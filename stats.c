92ccdc04a29d0b899dd73589560b170dc068193d
Lucas Vivian <lucviv68@gmail.com>
2023-03-29 12:03:56 -0300

    Update on stats.c

--- stats.c
+++ stats.c
@@ -33,12 +33,7 @@ void main() {
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-  
-  print_statistics();
+                                7,  87, 250, 230, "
   print_array();
   sort_array();
 
